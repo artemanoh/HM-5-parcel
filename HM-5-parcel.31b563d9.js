1bab0306d989377fd69494c67968b491fb75f459
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-04-13 10:39:32 +0300

    Updates

--- HM-5-parcel.31b563d9.js
+++ HM-5-parcel.31b563d9.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"5j6Kf":[function(require,module,exports,__globalThis) {
+})({"hst7a":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 55089;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "439701173a9199ea";
 var HMR_USE_SSE = false;
@@ -726,6 +726,6 @@ exports.export = function(dest, destName, get) {
     });
 };
 
-},{}]},["5j6Kf","a0t4e"], "a0t4e", "parcelRequired5ee", {})
+},{}]},["hst7a","a0t4e"], "a0t4e", "parcelRequired5ee", {})
 
 //# sourceMappingURL=HM-5-parcel.31b563d9.js.map
