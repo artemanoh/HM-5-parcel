b86ff33ea47e4c1e1828627beb477d6d219a841b
artemanoh <61391756+artemanoh@users.noreply.github.com>
2025-04-13 09:54:53 +0000

    Deploying to gh-pages from @ artemanoh/hm-5-parcel@0b58a414556583877daaed7effdf8c44bbb303bd 🚀

--- hm-5-parcel.e539bb17.js
+++ hm-5-parcel.e539bb17.js
@@ -1,2 +1,2 @@
 document.querySelector("#sum").textContent="Сума 10 + 5 = 15",document.querySelector("#subtract").textContent="Віднімання 10 - 5 = 5",document.querySelector("#multiply").textContent="Ділення 10 + 5 = 50",document.querySelector("#divide").textContent="Ділення 10 / 5 = "+2;
-//# sourceMappingURL=HM-5-parcel.d78272d4.js.map
+//# sourceMappingURL=hm-5-parcel.e539bb17.js.map
